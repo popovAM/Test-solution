0e1fa38e902ad55e9cde739e49a57cfebe34322b
Антон Попов <kemanton22@mail.ru>
2024-08-02 11:38:04 +0700

    Изменен action удаления склада

--- Solution.Module/Controllers/Storage_ListView.cs
+++ Solution.Module/Controllers/Storage_ListView.cs
@@ -19,7 +19,6 @@ using System.Text;
 
 namespace Solution.Module.Controllers
 {
-    // For more typical usage scenarios, be sure to check out https://documentation.devexpress.com/eXpressAppFramework/clsDevExpressExpressAppViewControllertopic.aspx.
     public partial class Storage_ListView : ViewController
     {
         public Storage_ListView()
@@ -72,27 +71,21 @@ namespace Solution.Module.Controllers
         /// <param name="e"></param>
         private void deleteStorage_Execute(object sender, SimpleActionExecuteEventArgs e)
         {
-            var collectionSource = new CollectionSource(ObjectSpace, typeof(Storage));
-            var view = Application.CreateListView("Storage_ListView", collectionSource, false);
-            
-            // Выборка склада по дате создания
-            var selectedStorage = view.SelectedObjects.OfType<Storage>().ToList();
+            Storage selectedStorage = (Storage)ObjectSpace.GetObject(View.CurrentObject);
 
             if (selectedStorage != null)
             {
-                foreach (var storage in selectedStorage) {
-                    bool isEmpty = true;
-                    foreach (var picket in storage.Pickets)
+                bool isEmpty = true;
+                foreach (var picket in selectedStorage.Pickets)
+                {
+                    if (picket.CargoPickets.Any())
                     {
-                        if (picket.CargoPickets.Any())
-                        {
-                            isEmpty = false;
-                            break;
-                        }
+                        isEmpty = false;
+                        break;
                     }
-                    if (isEmpty == true) ObjectSpace.Delete(storage);
-                    else throw new UserFriendlyException("На пикетах находятся грузы.");
                 }
+                if (isEmpty == true) ObjectSpace.Delete(selectedStorage);
+                else throw new UserFriendlyException("На пикетах находятся грузы.");
             }
             //Сохранение изменений
             ObjectSpace.CommitChanges();
